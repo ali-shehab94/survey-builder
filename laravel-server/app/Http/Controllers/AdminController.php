858edcf2500285e53ef82f25b27ec69874f3afcd
ali-shehab94 <ali.shehab.ut@gmail.com>
2022-06-17 21:11:20 +0300

    major bug fix in admin controller file

--- laravel-server/app/Http/Controllers/AdminController.php
+++ laravel-server/app/Http/Controllers/AdminController.php
@@ -14,39 +14,47 @@ class AdminController extends Controller
     public function createSurvey(Request $request)
     {
 
-        // dd($request->questions);
+        // we start with the survey title and create it in the database
         $survey = new Survey();
         $survey->title = $request->title;
         $survey->save();
+        // the key questions hold all the questions added with attributes "type", "content", and "choices"
+        // iterating through all the questions and recording them one by one in the database
         $questions = $request->questions;
         foreach ($questions as $question) {
 
             $type = $question['type'];
             $content = $question['content'];
+            // if the question has a choices attribute, it will be stored in a variable that we will iterate through later
+            if (isset($question['choices'])){
+                $choices = $question['choices'];
+            }else {
+                $choices = null;
+            }
             $question = new Question();
             $question->survey_id = $survey->id;
             $question->type = $type;
             $question->content = $content;
             $question->save();
-            // dd($question->id);
             
-            $choices = $question['choices'];
+            // storing question id in a variable for the use of choices
             $question_id = $question->id;
-            foreach ($choices as $choice) {
-                // dd($choice);
-                $choice_content = $choice;
-                $choice = new Choice();
-                $choice->question_id = $question_id;
-                // dd($choice);
-                $choice->choice = $choice_content;
-                $choice->save();
+            // iterating over choices and recording them one by one in the database
+            if ($choices){
+                foreach ($choices as $choice) {
+                    $choice_content = $choice;
+                    $choice = new Choice();
+                    $choice->question_id = $question_id;
+                    $choice->choice = $choice_content;
+                    $choice->save();
+                }
             }
         }
         return response()->json([
-            'status' => 'success',
-            'message' => 'Successfully created survey',
-            'survey_title' => $survey->title,
-            'surevey_id' => $survey->id
+            'Status' => 'Success',
+            'Message' => 'Successfully created survey',
+            'Survey_title' => $survey->title,
+            'Surevey_id' => $survey->id
         ]);
     }
 }
